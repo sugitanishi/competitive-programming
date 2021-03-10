b8977a72decfe3ab4ae36b9c73490572ea3c6f24
sugitanishi <keita.abi.114@gmail.com>
2021-03-10 23:33:04 +0900

    AC

---
+++ atcoder/agc052/a.py
@@ -0,0 +1,17 @@
+#☆𝒐𝒎𝒂𝒋𝒊𝒏𝒂𝒊☆#
+import sys
+import math
+import itertools
+from functools import lru_cache
+from collections import deque
+sys.setrecursionlimit(10000000)
+input=lambda : sys.stdin.readline().rstrip()
+'''''✂'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+n=int(input())
+for i in range(n):
+    x=int(input())
+    print('1'*x+'0'*x+'1')
+    input()
+    input()
+    input()
